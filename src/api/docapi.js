f0484e849a4e0548992d05ed24427acdcbc3d7b4
6 <1057117849@qq.com>
2023-04-16 20:27:37 +0800

    @version 2.1.0 @User qc @Content 文集列表 @time 2023-04-16 20:27:33

--- src/api/docapi.js
+++ src/api/docapi.js
@@ -1,5 +1,8 @@
 import requestfordoc from '../http/requestfordoc';
 
+
+// doc接口整合下，方面独立成项目,首先不要写代码，得先更新，不然白写
+
 export function docsadd(title,content,proj_id) {
   return requestfordoc({
     url:'/docs/add',
---
+++ src/views/doc/docprojlist.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  文集列表
+</div>
+</template>
+
+<script>
+export default {
+  name: "docprojlist"
+};
+</script>
+
+<style scoped>
+
+</style>
